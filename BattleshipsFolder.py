ca80b28bd0198a5edbd36f0ff0e48ed90f38b093
cartesian4 <simon.rawk@gmail.com>
2026-03-05 19:05:24 +1100

    Additional point system. Only printed at the end of the game.

--- BattleshipsFolder.py
+++ BattleshipsFolder.py
@@ -1,10 +1,10 @@
 import random as rand
-from random import randrange
+import pygame as py
 
 playing = True
 
 ship_initial = ["B|", "C|", "F|", "A|", "S|"]
-ship_names = ["Battleship", "Cruiser", "Frigate", "Aircraft Carrier", "Sub"]
+ship_names = ["Battleship", "Cruiser", "Frigate", "Aircraft Carrier", "Submarine"]
 number_of_ships = len(ship_initial)
 
 def get_username():
@@ -39,84 +39,29 @@ def viable_location_comp(row, col):
     return (0 <= row < map_size and 0 <= col < map_size and (row, col) not in opp)
 
 def player_ship_coordinate(player_board, occupied):
-    """
-    function for player placement ship
-    """
-    while True:
+    battleship_count = 0
+    ship_value = [None]*(number_of_ships)
+    #Function for player placement ship
+    while battleship_count <= number_of_ships-1:
         try:
-            row = int(input("Enter the row for Battleship: "))-1
-            col = int(input("Enter the column for Battleship: "))-1
+            row = int(input(f"Enter the row for the {ship_names[battleship_count]}: "))-1
+            col = int(input(f"Enter the column for the {ship_names[battleship_count]}: "))-1
 
             if viable_location(row, col):
-                player_board[row][col] = "B|"
+                player_board[row][col] = ship_initial[battleship_count]
                 occupied.add((row, col))
-                break
+                battleship_count = battleship_count+1
             else:
                 print("Invalid coordinates. Please enter correct value.")
         except ValueError:
             print("Invalid input. Please enter a valid integer.")
-
-    while True:
-        try:
-            row = int(input("Enter the row for Cruiser: "))-1
-            col = int(input("Enter the column for Cruiser: "))-1
-
-            if viable_location(row, col):
-                player_board[row][col] = "C|"
-                occupied.add((row, col))
-                break
-            else:
-                print("Invalid coordinates. Please enter correct values.")
-        except ValueError:
-            print("Invalid input. Please enter a valid integer.")
-
-    while True:
-        try:
-            row = int(input("Enter the row for Frigate: "))-1
-            col = int(input("Enter the column for Frigate: "))-1
-
-            if viable_location(row, col):
-                player_board[row][col] = "F|"
-                occupied.add((row, col))
-                break
-            else:
-                print("Invalid coordinates. Please enter correct values")
-        except ValueError:
-            print("Invalid input. Please enter a valid integer.")
-
-    while True:
-        try:
-            row = int(input("Enter the row for Aircraft Carrier: "))-1
-            col = int(input("Enter the column for Aircraft Carrier: "))-1
-
-            if viable_location(row, col):
-                player_board[row][col] = "A|"
-                occupied.add((row, col))
-                break
-            else:
-                print("Invalid coordinates. Please enter correct values")
-        except ValueError:
-            print("Invalid input. Please enter a valid integer.")
-
-    while True:
-        try:
-            row = int(input("Enter the row for Submarine: "))-1
-            col = int(input("Enter the column for Submarine: "))-1
-
-            if viable_location(row, col):
-                player_board[row][col] = "S|"
-                occupied.add((row, col))
-                break
-            else:
-                print("Invalid coordinates. Please enter correct values")
-        except ValueError:
-            print("Invalid input. Please enter a valid integer.")
+        #Increases the battleship value (from array) being printed.
 
     return player_board, occupied
 
 
 def comp_ship_coordinate(comp_board):
-    # Picks 5 values between 0 and map_size * map_size and creates opponent board
+    # Picks values between 0 and map_size * map_size and creates opponent board
     unique_array = rand.sample(range(0, map_size * map_size), len(ship_initial))
     for position in unique_array:
         x = position // map_size
@@ -140,40 +85,51 @@ def get_input_from_player():
             print("Invalid input. Please enter a valid integer.")
     return row, col
 
-def check_player_hit(comp_board, player_hit, row, col):
+def check_player_hit(comp_board, player_hit, row, col, player_points):
+    player_points = 0
     #Player hit or missed on enemy ship
     if comp_board[row][col] == "X|":
         player_hit[row][col] = "H|"
+        player_points = player_points + 10
         print("Computer Ship has been hit!")
     else:
         player_hit[row][col] = "M|"
+        player_points = player_points - 1
         print("You missed!")
-    return player_hit
+    print(f"Your points: {player_points}")
+    return player_hit, player_points
 
-def check_comp_hit(player_board, comp_hit, row, col):
+def check_comp_hit(player_board, comp_hit, row, col, comp_points):
     #Whether the computer hit or missed the player ship
     print(f"Computer guessed: Row {row+1}, Column {col+1}")
     if player_board[row][col] == "B|":
         comp_hit[row][col] = "B|"
+        comp_points = comp_points + 10
         print("Player Battleship has been hit!")
     elif player_board[row][col] == "C|":
         comp_hit[row][col] = "C|"
+        comp_points = comp_points + 10
         print("Player Cruiser has been hit!")
     elif player_board[row][col] == "F|":
         comp_hit[row][col] = "F|"
+        comp_points = comp_points + 10
         print("Player Frigate has been hit!")
     elif player_board[row][col] == "A|":
         comp_hit[row][col] = "A|"
+        comp_points = comp_points + 10
         print("Player Aircraft carrier has been hit!")
     elif player_board[row][col] == "S":
         comp_hit[row][col] = "S|"
+        comp_points = comp_points + 10
         print("Player Sub has been hit!")
 
     else:
         comp_hit[row][col] = "M|"
         print("Opponent missed!")
+        comp_points = comp_points - 1
     player_board[row][col] = "*|"
-    return comp_hit
+    print(f"Opponent points: {comp_points}")
+    return comp_hit, comp_points
 
 def play_again():
     while True:
@@ -220,7 +176,9 @@ while playing:
     occupied = set()
     shots = set()
     opp = set()
-    guesses = 25
+    guesses = map_size**2
+    player_points = 0
+    comp_points = 0
 
     player_ship_coordinate(player_board, occupied)
     print("\nPlayer's board:")
@@ -230,13 +188,12 @@ while playing:
         #Player's turn
         print("\nIt's your turn to guess!")
         #Pluralization for guesses left
-        print(guesses)
         if guesses > 1:
             print(f"You have {guesses} guesses left.")
         elif guesses == 1:
             print(f"You have {guesses} guess left.")
         row, col = get_input_from_player()
-        check_player_hit(comp_board, player_hit, row, col)
+        player_points = check_player_hit(comp_board, player_hit, row, col, player_points)
         guesses = guesses-1
         print("\nYour guesses so far:")        
         display_battlefield(player_hit)
@@ -246,7 +203,7 @@ while playing:
         position = comp_shots[guesses]
         row = position // map_size
         col = position % map_size
-        check_comp_hit(player_board, comp_hit, row, col)
+        comp_points = check_comp_hit(player_board, comp_hit, row, col, comp_points)
         display_battlefield(player_board)
 
         #Count number of ships hit
@@ -257,17 +214,20 @@ while playing:
         if guesses == 0:
             print("\nYou've used all your guesses.")
             if player_ships_hit == comp_ships_hit:
-                print("\nGame over! You have both sunk the same number of ships! It was a tie!")
+                print("\nGame over! You both sunk the same number of ships! It was a tie!")
                 break
             elif player_ships_hit < comp_ships_hit:
                 print("\nGame over! The computer has sunk more of your ships than you. You lose!")
                 break
-            if player_ships_hit > comp_ships_hit:
+            elif player_ships_hit > comp_ships_hit:
                 print("\nCongratulations! You've sunk more ships than the computer. You win!")
 
         #If all of board ships are gone, end turn loop
         allShipsGone = check_win(player_ships_hit, comp_ships_hit)
         if allShipsGone == True:
             guesses = 0
+        
+    print(f"\nYour points: {player_points}")
+    print(f"\nOpponent points: {comp_points}")
 
     playing = play_again()    
